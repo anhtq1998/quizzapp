5fc91ae3fe9b756da00a253dee2b30fc5101a91a
Tạ Quyền Anh <47656743+anhtq1998@users.noreply.github.com>
2022-07-26 09:12:10 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,285 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Animated, Image, Modal, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { COLORS, SIZES } from './app/styles/styles';
 
 export default function App() {
+  const [allQuestions, setAllQuestions] = useState([])
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+  const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
+  const [correctOption, setCorrectOption] = useState(null);
+  const [isOptionsDisabled, setIsOptionsDisabled] = useState(false);
+  const [score, setScore] = useState(0)
+  const [showNextButton, setShowNextButton] = useState(false)
+  const [showScoreModal, setShowScoreModal] = useState(false)
+
+  useEffect(() => {
+    //call apis
+    axios.get('https://62d8b52d9c8b5185c78b9936.mockapi.io/allQuess').then(res => {
+      setAllQuestions(res.data);
+      console.log(res.data);
+    })
+  }, [])
+
+  const validateAnswer = (selectedOption) => {
+    let correct_option = allQuestions[currentQuestionIndex]['correct_option'];
+    setCurrentOptionSelected(selectedOption);
+    setCorrectOption(correct_option);
+    setIsOptionsDisabled(true);
+    if (selectedOption == correct_option) {
+      setScore(score + 1)
+    }
+    setShowNextButton(true)
+  }
+  const handleNext = () => {
+    if (currentQuestionIndex == allQuestions.length - 1) {
+      setShowScoreModal(true)
+    } else {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setCurrentOptionSelected(null);
+      setCorrectOption(null);
+      setIsOptionsDisabled(false);
+      setShowNextButton(false);
+    }
+    Animated.timing(progress, {
+      toValue: currentQuestionIndex + 1,
+      duration: 1000,
+      useNativeDriver: false
+    }).start();
+  }
+  const restartQuiz = () => {
+    setShowScoreModal(false);
+
+    setCurrentQuestionIndex(0);
+    setScore(0);
+
+    setCurrentOptionSelected(null);
+    setCorrectOption(null);
+    setIsOptionsDisabled(false);
+    setShowNextButton(false);
+    Animated.timing(progress, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: false
+    }).start();
+  }
+
+  const renderQuestion = () => {
+    return (
+      <View style={{
+        marginVertical: 40
+      }}>
+        <View style={{
+          flexDirection: 'row',
+          alignItems: 'flex-end'
+        }}>
+          <Text style={{ color: COLORS.white, fontSize: 20, opacity: 0.6, marginRight: 2 }}>{currentQuestionIndex + 1}</Text>
+          <Text style={{ color: COLORS.white, fontSize: 18, opacity: 0.6 }}>/ {allQuestions.length}</Text>
+        </View>
+
+        <Text style={{
+          color: COLORS.white,
+          fontSize: 30
+        }}>{allQuestions[currentQuestionIndex]?.question}</Text>
+      </View>
+    )
+  }
+  const renderOptions = () => {
+    return (
+      <View>
+        {
+          allQuestions[currentQuestionIndex]?.options.map(option => (
+            <TouchableOpacity
+              onPress={() => validateAnswer(option)}
+              disabled={isOptionsDisabled}
+              key={option}
+              style={{
+                borderWidth: 3,
+                borderColor: option == correctOption
+                  ? COLORS.success
+                  : option == currentOptionSelected
+                    ? COLORS.error
+                    : COLORS.secondary + '40',
+                backgroundColor: option == correctOption
+                  ? COLORS.success + '20'
+                  : option == currentOptionSelected
+                    ? COLORS.error + '20'
+                    : COLORS.secondary + '20',
+                height: 60, borderRadius: 20,
+                flexDirection: 'row',
+                alignItems: 'center', justifyContent: 'space-between',
+                paddingHorizontal: 20,
+                marginVertical: 10
+              }}
+            >
+              <Text style={{ fontSize: 20, color: COLORS.white }}>{option}</Text>
+              {
+                option == correctOption ? (
+                  <View style={{
+                    width: 30, height: 30, borderRadius: 30 / 2,
+                    backgroundColor: COLORS.success,
+                    justifyContent: 'center', alignItems: 'center'
+                  }}>
+                    <MaterialCommunityIcons name="check" style={{
+                      color: COLORS.white,
+                      fontSize: 20
+                    }} />
+                  </View>
+                ) : option == currentOptionSelected ? (
+                  <View style={{
+                    width: 30, height: 30, borderRadius: 30 / 2,
+                    backgroundColor: COLORS.error,
+                    justifyContent: 'center', alignItems: 'center'
+                  }}>
+                    <MaterialCommunityIcons name="close" style={{
+                      color: COLORS.white,
+                      fontSize: 20
+                    }} />
+                  </View>
+                ) : null
+              }
+
+            </TouchableOpacity>
+          ))
+        }
+      </View>
+    )
+  }
+  const renderNextButton = () => {
+    if (showNextButton) {
+      return (
+        <TouchableOpacity
+          onPress={handleNext}
+          style={{
+            marginTop: 20, width: '100%', backgroundColor: COLORS.accent, padding: 20, borderRadius: 5
+          }}>
+          <Text style={{ fontSize: 20, color: COLORS.white, textAlign: 'center' }}>Next</Text>
+        </TouchableOpacity>
+      )
+    } else {
+      return null
+    }
+  }
+
+
+  const [progress, setProgress] = useState(new Animated.Value(0));
+
+  const progressAnim = progress.interpolate({
+    inputRange: [0, allQuestions.length],
+    outputRange: ['0%', '100%']
+  })
+
+  const renderProgressBar = () => {
+    return (
+      <View style={{
+        width: '100%',
+        height: 20,
+        borderRadius: 20,
+        backgroundColor: '#00000020',
+
+      }}>
+        <Animated.View style={[{
+          height: 20,
+          borderRadius: 20,
+          backgroundColor: COLORS.accent
+        }, {
+          width: progressAnim
+        }]}>
+
+        </Animated.View>
+
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{
+      flex: 1
+    }}>
+      <StatusBar barStyle="light-content" backgroundColor={COLORS.primary} />
+      <View style={{
+        flex: 1,
+        paddingVertical: 70,
+        paddingHorizontal: 16,
+        backgroundColor: COLORS.background,
+        position: 'relative'
+      }}>
+
+        {renderProgressBar()}
+
+        {renderQuestion()}
+
+        {renderOptions()}
+
+        {renderNextButton()}
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={showScoreModal}
+        >
+          <View style={{
+            flex: 1,
+            backgroundColor: COLORS.primary,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}>
+            <View style={{
+              backgroundColor: COLORS.white,
+              width: '90%',
+              borderRadius: 20,
+              padding: 20,
+              alignItems: 'center'
+            }}>
+              <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{score > (allQuestions.length / 2) ? 'Congratulations!' : 'Oops!'}</Text>
+
+              <View style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                marginVertical: 20
+              }}>
+                <Text style={{
+                  fontSize: 30,
+                  color: score > (allQuestions.length / 2) ? COLORS.success : COLORS.error
+                }}>{score}</Text>
+                <Text style={{
+                  fontSize: 20, color: COLORS.black
+                }}>/ {allQuestions.length}</Text>
+              </View>
+              <TouchableOpacity
+                onPress={restartQuiz}
+                style={{
+                  backgroundColor: COLORS.accent,
+                  padding: 20, width: '100%', borderRadius: 20
+                }}>
+                <Text style={{
+                  textAlign: 'center', color: COLORS.white, fontSize: 20
+                }}>Retry Quiz</Text>
+              </TouchableOpacity>
+
+            </View>
+
+          </View>
+        </Modal>
+
+        <Image
+          source={require('./assets/DottedBG.png')}
+          style={{
+            width: SIZES.width,
+            height: 130,
+            zIndex: -1,
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            opacity: 0.5
+          }}
+          resizeMode={'contain'}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
